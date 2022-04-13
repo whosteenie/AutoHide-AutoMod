a408c89df4f2c4250063ee6614eae4985930ade3
Justin <whosteenie@gmail.com>
2022-04-13 04:27:12 -0700

    Detect when user activates Reddit tab.

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        if (/^https:\/\/www\.reddit/.test(current_tab_info.url)) {
+            chrome.scripting.executeScript({target: {tabId: tab.tabId}, files: ['foreground.js']}, () => console.log('i injected'))
+        }
+    });
+});
---
+++ foreground.js
@@ -0,0 +1,5 @@
+console.log('from foreground')
+
+//document.getElementsByClassName("._1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")
+
+$("._1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4").remove()
